e08e870ea3b90fff961ee448948f36dc622fa6a7
MERT ERNAZ <rnzappdroid@gmail.com>
2023-06-29 21:13:51 +0300

    barcode read and write

---
+++ BarcodeGenerator/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BarcodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(598, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "READ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(598, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(307, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 131);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save Barcode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(598, 106);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(395, 131);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Barkod:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Barkod Tipi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BarcodeGenerator/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Barcode;
+
+
+namespace BarcodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        BarcodeSettings bs = new BarcodeSettings();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Array bTypes=Enum.GetValues(typeof(BarCodeType));
+            comboBox1.DataSource = bTypes;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //BarcodeSettings bs = new BarcodeSettings();
+            bs.Type = BarCodeType.Code25;
+            bs.Data = "ABC 12330";
+            
+            BarCodeGenerator bg = new BarCodeGenerator(bs);
+            bg.GenerateImage().Save("Code25bAR.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            System.Diagnostics.Process.Start("Code25bAR.jpeg");
+            textBox1.Text = bs.Data;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string barS = comboBox1.SelectedItem.ToString();           
+           
+            bs.Type= (BarCodeType)Enum.Parse(typeof(BarCodeType), barS);
+            
+            if (textBox1.Text.ToString() == "")
+            {
+                bs.Data = "ABC 123456789";
+                textBox1.Text = bs.Data;
+            }
+            else
+            {
+                bs.Data = textBox1.Text.ToString();
+            }
+            BarCodeGenerator bg = new BarCodeGenerator(bs);
+            pictureBox1.Image = bg.GenerateImage();
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)//Barcode Image kaydet
+        {
+            BarCodeGenerator bg = new BarCodeGenerator(bs);
+            string barS = comboBox1.SelectedItem.ToString();
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bitmap Image(*.bmp)|*.bmp|JPEG Image(*.jpg)|*.jpg|PNG Image(*.png)|*.png";
+            saveFileDialog.Title = "Barkod nereye kaydedilsin ?";
+            string filename = textBox1.Text + "-" + barS;
+            saveFileDialog.FileName = filename;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bg.GenerateImage().Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Dosyadan barkod oku
+        {
+            OpenFileDialog ofd=new OpenFileDialog();
+            ofd.Filter = "Bitmap Image(*.bmp)|*.bmp|JPEG Image(*.jpg)|*.jpg|PNG Image(*.png)|*.png";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image=Image.FromFile(ofd.FileName);
+                string readBarcode = BarcodeScanner.ScanOne(pictureBox2.Image as Bitmap);
+                
+                textBox2.Text = readBarcode;
+                // bs.Type = (BarCodeType)Enum.Parse(typeof(BarCodeType),readBarcode);
+               // textBox2.Text = bs.Type.ToString();
+            }
+        }
+    }
+}
